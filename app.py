9e488bcb80c5c8ccee6cdae40fa5e0ba4d339751
sds <sds@sds-PC>
2016-11-02 17:30:26 +0900

    Except error, Comment about Code

--- app.py
+++ app.py
@@ -2,23 +2,44 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+import json
 
 app = Flask(__name__)
 
+success_message = {'status':'success','message':'received'}
+failed_message = {'status':'failed','message':'received'}
+
+'''
+1. Request Body Parsing
+2. Validate Content
+3. Send Response
+'''
 @app.route("/job", methods=["POST"])
-def create_job():   
+def create_job():
     try:
-        contents = request.json # contents = GET JSON  
-        print(contents["id"])
-        dic = {'id':'nam'}
-        list = [
-            {'param':'f','val':2},
-            {'param':'b','val':10}
-            ]
+        # 1. Request Body Parsing
+        contents = request.json
+        id = contents["id"]
+        script = contents["script"]
+        type = contents["type"]
+
     except KeyError as e:
-        print("Json  error")
-        
-    return jsonify(results=dic)
+        print("Key error")
+        return json.dumps(failed_message)
+
+    else:    
+        '''
+        2. Validate Content
+        must be id / script /type 
+        id     : integer
+        script : 5000 byte
+        type   : batch / bash / ksh / csh / python
+        '''
+        if(id and script and type):
+            # 3. Send Response
+            return json.dumps(success_message)
+        else:
+            return json.dumps(failed_message)
 
 if __name__ == "__main__":
    app.run()
