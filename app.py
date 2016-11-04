ba6247d4400f1ca7293058b7fc5da8658cdafd4c
sds <sds@sds-PC>
2016-11-04 14:01:18 +0900

    code merge plz

--- app.py
+++ app.py
@@ -7,16 +7,24 @@ import json
 import subprocess
 import time
 import requests
+import psutil
+from psutil import disk_partitions
+import os
+
 
 app = Flask(__name__)
 app.config.from_pyfile('config.properties')
+
+PATH = app.config["PATH"]
+URL = app.config["URL"]
+POSTURL = app.config["POSTURL"]
 
 success_message = {'status':'success','message':'received'}
 failed_message = {'status':'failed','message':'received'}
 
     
 '''
-    cmd 명령어 실행
+    cmd command run
 '''
 def subprocess_open(cmd):
     popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr = subprocess.PIPE)
@@ -24,35 +32,33 @@ def subprocess_open(cmd):
     return stdoutdata, stderrdata
 
 '''
+            
 1. Request Body Parsing
 2. Validate Content
 3. Send Response
+4. received dictionary from client 
+5. Validate Value type
+6. execute bat 
+7. Send post result    
+
 '''
 
 
+
+''' execute batch file'''
 def SaveFileAndRun(dict):
-    
     
     yesBatch = {'Status':'Success','Message':'Create File'}
     noBatch = {'Status':'Fail','Message':'Creation Error Occured'}
     
-    ex_dic1 = {'Status':'Success','Message':'dir','Type':'bat'}
-    ex_dic2 = {'Status':'Success','Message':'dir','Type':'pirlo'}
-    
-    '''
-            클라이언트로부터 받은 dictionary를 파라미터로 받음.  
-            딕셔너리의 Type값이 bat일 경우 매세지의 내용을 저장하고 성공 메세지를 반환
-            딕셔너리의 Type값이 bat이 아닐 경우 별도의 과정 없이 실패 메세지를 반환
-    '''
-    
-    PATH = app.config["PATH"]
-    URL = app.config["URL"]
     fileName = time.strftime("%Y%m%d_%H%M%S", time.localtime())
-    print(dict["type"])
+    '''file name is 'YearMonthDay_HoursMinuteSeconds'.'''
+    
+    #5. Validate Value type
     if (dict["type"]=='bat'):
         try :  
             savefilePointer = open(PATH + fileName + '.bat','w')
-            
+            #save 실패, naming ,
             fileContent = dict["script"]
             savefilePointer.write(fileContent)
             
@@ -67,18 +73,62 @@ def SaveFileAndRun(dict):
         finally:
             savefilePointer.close()
             
-        #.bat 실행  
+        #6. execute bat  
         if response.text == "GOOD":
-            print(subprocess_open(PATH + fileName + '.bat')[0])
+            post_result = {}
+            pcs_result = subprocess_open(PATH + fileName + '.bat')
             
+             #7. Send post result   
+            if len(pcs_result[1]) == 0:
+                post_result['status'] = "success_excute"
+                post_result['message'] = pcs_result[0]
+                response = requests.request("POST", POSTURL, data=json.dumps(post_result, ensure_ascii=False), verify=False)
+                return post_result['message']
+            else:
+                post_result['status'] = "failed_excute"
+                post_result['message'] = pcs_result[1]    
+                response = requests.request("POST", POSTURL, data=json.dumps(post_result, ensure_ascii=False), verify=False)
+                return post_result['message']
         else:
-            print ("Connection Error")  
+            print ("Connection Error")   
             
     else:
          response = requests.request("PUT", URL + fileName, data=json.dumps(noBatch), verify=False)
-             
-             
+         return noBatch["Status"]
+         
 
+
+
+@app.route("/storage2", methods=["POST"])
+def diskthread():
+        diskinfo()
+             
+@app.route("/storage", methods=["POST"])
+def diskinfo():
+    
+    os.system("python test.py")
+    subprocess.Popen("python test.py", shell = True)
+    
+    disk_list=[]
+    disk_dictionary = {'usage':''}
+    disk_dictionary['mountpoint']=''
+
+    for i, disk in enumerate(disk_partitions()):
+        mountpoint_value = disk.mountpoint     
+        usage_value = str(psutil.disk_usage(disk.mountpoint).percent)       
+        disk_dictionary['mountpoint'] = mountpoint_value
+        disk_dictionary['usage'] = usage_value
+        disk_list.append(disk_dictionary)
+        
+    return jsonify(results=disk_list)
+    
+    
+    '''Storage_Usage_result = psutil.disk_partitions(device)
+    print Storage_Usage_result
+    string_storage = str(Storage_Usage_result)
+    split_storage = string_storage.split("device=")'''
+    
+    
 @app.route("/job", methods=["POST"])
 def create_job():
     try:
@@ -100,15 +150,18 @@ def create_job():
         script : 5000 byte
         type   : batch / bash / ksh / csh / python
         '''
+        
         if(id and script and type):
-            # 3. Send Response
+            # 3. Send Response            
+            response = requests.request("PUT", URL+id, data=json.dumps(success_message), verify=False)
             
+            # 4.received dictionary from client 
+            return_value=SaveFileAndRun(contents)
+            return return_value
             
-            response = requests.request("PUT", URL + fileName, data=json.dumps(success_message), verify=False)
-            SaveFileAndRun(contents)
         else:
-            response = requests.request("PUT", URL + fileName, data=json.dumps(failed_message), verify=False)
-
+            response = requests.request("PUT", URL+id, data=json.dumps(failed_message), verify=False)
+            return jsonify(results=failed_message)
 
     
 if __name__ == "__main__":
